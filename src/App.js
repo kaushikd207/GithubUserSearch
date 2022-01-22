62c7653260150f855d932cf792f516c9ed0f4e0d
kaushikd207 <kaushikd207@gmail.com>
2022-01-22 15:58:15 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Search from "./components/Search";
+function App() {
+  return (
+    <Search/>
+    
+  );
+}
+
+export default App;
