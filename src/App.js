244bb4d6b9c49861c33ac646cb6ee3fe3f603908
kaushikd207 <kaushikd207@gmail.com>
2022-02-03 23:46:27 +0530

    added userdata

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./components/Search";
+import UserCard from './components/User';
 import React from "react";
-class App extends React.Component{
-    state={
-        user:null
+class App extends React.Component {
+    state = {
+        user: null,
+        error: null,
+        loading: false,
     };
-   fetchUserData = async username=>{
-       try{
-           const res=await fetch(`https://api.github.com/users/${username}`);
-           if(res.ok){
-               const data=await res.json();
-               return this.setState({
-                   user:data
-               });
-           }
-       }catch(arr){
-           console.log(arr);
-       }
-   };
-    render(){
-       return <Search fetchData={this.fetchUserData}/>;
+    fetchUserData = async username => {
+        this.setState({loading:true},async() =>{
+        try {
+            const res = await fetch(`https://api.github.com/users/${username}`);
+            if (res.ok) {
+                const data = await res.json();
+                return this.setState({
+                    user: data,
+                    loading:false,
+                });
+            }
+            const error = (await res.json()).message;
+            this.setState({
+                error,
+                loading:false,
+            });
+        } catch (err) {
+            this.setState({
+                error: "Ther was some error",
+                loading:false,
+            });
+        }
+    });
+};
+    render() {
+        const { error,loading,user } = this.state;
+        return (
+            <div>
+                <Search fetchData={this.fetchUserData} />
+                <div className='text-center pt-5'>
+                {loading &&<p>Loading...</p>}
+                {error && <p className='text-danger'>{error}</p>}
+                {!loading && !error && <UserCard user={user}/> }
+                </div>
+            </div>
+        );
     }
 }
 export default App;
